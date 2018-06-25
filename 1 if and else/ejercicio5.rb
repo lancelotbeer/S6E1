86b3ccd67a6edd0760a792a03525f9f637f44e20
JuanRaba <jrabanillog@gmail.com>
2018-06-25 12:44:58 -0400

    Add ejer1

--- 1 if and else/ejercicio5.rb
+++ 1 if and else/ejercicio5.rb
@@ -1,15 +1,19 @@
 # Utiliza álgebra booleana para hacer un refactoring de este código
 # Para verificar la respuestas, puedes variar los valores de a y b.
 
-a = true
+a = false
 b = true
 
-if a == true
-  if b == true
+if a
+  if b
     puts 'Lograste A y B!'
   else
     puts 'Lograste A! Pero no B!'
   end
 else
-  puts 'No lograste A ni B!'
+  if b
+	puts 'No lograste A! Pero si B!'
+  else
+	puts 'No lograste A ni B!'
+  end
 end
--- 1 if and else/ejercicio6.rb
+++ 1 if and else/ejercicio6.rb
@@ -1,13 +1,13 @@
 # Utiliza álgebra booleana para hacer un refactoring de este código
 # Para verificar la respuestas, puedes modificar los valores de a y b.
 
-a = 'verdadero'
-b = 'falso'
+a = true
+b = false
 
-if a == 'verdadero'
+if a
     puts ':)'
 else
-  if b == 'verdadero'
+  if b
     puts ':|'
   else
     puts ':('
